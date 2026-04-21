3f7eeae9ee6dc25cb1117c1489d08956eeb1509f
nicole <nicole@allyou.srl>
2026-04-21 10:09:17 +0200

    color changes

--- src/components/ScrollImages.jsx
+++ src/components/ScrollImages.jsx
@@ -2,18 +2,9 @@ import{ useEffect, useState, useRef } from 'react';
 
 
 const images = [
-  '/img1.jpg',
-  '/img2.jpg',
-  '/img3.jpg',
-  '/img4.jpg',
-    '/img5.jpg',
-      '/img6.jpg',
-        '/img7.jpg',
-          '/img8.jpg',
-            '/img9.jpg', 
-             '/img10.jpg',
-               '/img11.jpg',
-                 '/img12.jpg'
+
+            '/img11.jpg', 
+         
 ];
 
 function ScrollImages() {
